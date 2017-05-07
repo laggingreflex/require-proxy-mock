b85602ac899946249d33faff5fd05385bd5300a2
laggingreflex <laggingreflex@gmail.com>
2017-05-07 11:42:20 +0530

    Revert "fail: ghost"
    This reverts commit d07fc5692676babcbb56303f81c9ca77fbf2a867.

--- index.js
+++ index.js
@@ -1,57 +1,32 @@
 const Module = require('module');
 
 const _load = Module._load;
-Module._load = requireProxyMock;
+Module._load = requireProxyMockPatchLoad;
 
-const isGhost = global.requireProxyMockIsGhostSymbol = global.requireProxyMockIsGhostSymbol || Symbol('requireProxyMockIsGhostSymbol');
+const mocks = module.exports = global.requireProxyMocks = global.requireProxyMocks || {};
 
-const mocks = module.exports = global.requireProxyMocks = global.requireProxyMocks || new Proxy({}, {
-  get(mocks, request) {
-    if (request === 'default') {
-      return mocks;
-    } else if (request === '__esModule') {
-      return true;
-    } else if (request === 'isGhost') {
-      return isGhost;
-    } else {
-      return mocks[request] || (mocks[request] = {
-        [isGhost]: true
-      })
-    }
-  },
-});
-
-function requireProxyMock(request) {
+function requireProxyMockPatchLoad(request) {
   const original = _load.apply(Module, arguments);
 
   if (typeof original !== 'object') {
     return original;
   }
 
-  if (request === 'require-proxy-mock') {
-    return original;
-  }
-
-  const getMock = () => {
-    const mock = mocks[request];
-    if (mock && !mock[isGhost]) {
-      return mock;
-    } else {
-      return original;
-    }
-  }
-
   return new Proxy(original, {
     apply(original, _this, _args) {
-      const fn = getMock();
+      const fn = mocks[request] || original;
       return fn.apply(_this, _args);
     },
     construct(original, _args) {
-      const cn = getMock();
+      const cn = mocks[request] || original;
       return new(cn.bind.apply(cn, _args));
     },
     get(original, name) {
       return mocks[request] && mocks[request][name] || original[name];
     },
+    set(original, prop, value) {
+      original[prop] = value;
+      return true;
+    }
   });
 }
