52a15831d345fb081e4c9957f6cbeaad20eedb33
laggingreflex <laggingreflex@gmail.com>
2018-09-03 07:40:49 +0530

    feat: Allow string, object, and Map

--- index.js
+++ index.js
@@ -4,7 +4,43 @@ require('./init')();
 
 exports.mocks = mocks;
 
-exports.mock = (key, value) => {
+exports.mock = (...args) => {
+  if (args.length === 1) {
+    if (args[0] instanceof Map) {
+      return exports.mockByMap(...args);
+    } else {
+      return exports.mockByObject(...args);
+    }
+  } else if (args.length === 2) {
+    return exports.mockSingle(...args);
+  } else {
+    throw new Error('Unexpected args');
+  }
+};
+
+exports.mockByObject = obj => {
+  for (const key in obj) {
+    mocks.set(key, obj[key]);
+  }
+  return () => {
+    for (const key in obj) {
+      mocks.delete(key);
+    }
+  };
+};
+
+exports.mockByMap = map => {
+  for (const [key, value] of map) {
+    mocks.set(key, value);
+  }
+  return () => {
+    for (const [key] of map) {
+      mocks.delete(key);
+    }
+  };
+};
+
+exports.mockSingle = (key, value) => {
   mocks.set(key, value);
   return () => mocks.delete(key);
 };
